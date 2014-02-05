30e572813ac1cf236146833fe79c123ff104580d
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 12:05:33 -0800

    added rest of functions

--- arithmetic.py
+++ arithmetic.py
@@ -11,13 +11,13 @@ def divide(num1, num2):
     return num1 / num2
 
 def square(num1):
-    pass
+    return num1 ** 2
 
 def cube(num1):
-    pass
+    return num1 ** 3
 
 def power(num1, num2):
-    pass
+    return num1 ** num2
 
 def mod(num1, num2):
-    pass
+    return num1 % num2
