e22d72fbb0669af7710eef6ac228edee0e61ac7b
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 12:03:00 -0800

    added subtract multiply divide

--- arithmetic.py
+++ arithmetic.py
@@ -2,13 +2,13 @@ def add(num1, num2):
     return num1 + num2
 
 def subtract(num1, num2):
-    pass
+    return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    return num1 / num2
 
 def square(num1):
     pass
